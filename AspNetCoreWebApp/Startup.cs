d08297f216702af65d5c4f32c7b7fd1160761bfb
hbncod <hbncod@gmail.com>
2021-07-07 10:20:30 -0300

    Atualização da projeto para .Net 5

--- AspNetCoreWebApp/Startup.cs
+++ AspNetCoreWebApp/Startup.cs
@@ -1,19 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using AspNetCoreWebApp.CloudStorage;
+using AspNetCoreWebApp.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.EntityFrameworkCore;
-using AspNetCoreWebApp.Models;
-using System.Globalization;
-using Microsoft.AspNetCore.Localization;
-using AspNetCoreWebApp.CloudStorage;
+using Microsoft.Extensions.Hosting;
 
 namespace AspNetCoreWebApp
 {
@@ -26,18 +20,16 @@ namespace AspNetCoreWebApp
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.Configure<CookiePolicyOptions>(options =>
             {
-                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
                 options.CheckConsentNeeded = context => true;
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
 
 
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Latest);
 
             services.AddDbContext<TvShowsContext>(options =>
                     options.UseSqlServer(Configuration.GetConnectionString("TvShowsContext")));
@@ -45,8 +37,7 @@ namespace AspNetCoreWebApp
             services.AddSingleton<ICloudStorage, GoogleCloudStorage>();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
@@ -55,7 +46,6 @@ namespace AspNetCoreWebApp
             else
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
@@ -63,25 +53,12 @@ namespace AspNetCoreWebApp
             app.UseStaticFiles();
             app.UseCookiePolicy();
 
-            /*var defaultCulture = new CultureInfo("en-US");
-            var localizationOptions = new RequestLocalizationOptions
-            {
-                DefaultRequestCulture = new RequestCulture(defaultCulture),
-                SupportedCultures = new List<CultureInfo> { defaultCulture },
-                SupportedUICultures = new List<CultureInfo> { defaultCulture },
+            app.UseRouting();
 
-            };
-            app.UseRequestLocalization(localizationOptions);
-            var ui = System.Globalization.CultureInfo.CurrentUICulture;*/
-
-            app.UseMvc(routes =>
+            app.UseEndpoints(cfg =>
             {
-                routes.MapRoute(
-                    name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                cfg.MapDefaultControllerRoute();
             });
-
-
         }
     }
 }
